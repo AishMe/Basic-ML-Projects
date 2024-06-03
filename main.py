34e80148e706a8dc670d0c68e697537fea46796e
Aishwarya Anil Menon <aishwarya.menon.15@gmail.com>
2024-06-03 10:11:59 +0530

    Saved the Model and Deployed it on Streamlit App

---
+++ main.py
@@ -0,0 +1,39 @@
+# Import required libraries
+import numpy as np
+import streamlit as st
+import cv2
+import keras
+from keras.models import load_model
+
+# Loading the model
+model = load_model('dog_breed.h5')
+
+# Name of Classes
+BREEDS = ['Scottish Deerhound', 'Maltese Dog', 'Afgan Hound']
+
+# Title of the app
+st.title("Dog Breed Prediction")
+st.markdown("Upload an image of the dog")
+
+# Uploading input image (dog)
+dog_image = st.file_uploader("Choose an image...", type="png")
+submit = st.button('Predict')
+
+# If the user clicks on submit, do this:
+if submit:
+    if dog_image is not None:
+
+        # Convert the file to an opencv image
+        file_bytes = np.asarray(bytearray(dog_image.read()), dtype=np.uint8)
+        opencv_image = cv2.imdecode(file_bytes, 1)
+
+        # Display the image
+        st.image(opencv_image, channels="BGR")
+        # Resizing the image
+        opencv_image = cv2.resize(opencv_image, (224,224))
+        # Convert image to 4 dimension
+        opencv_image.shape = (1,224,224,3)
+        # Make Prediction
+        Y_pred = model.predict(opencv_image)
+
+        st.title(str(f"The Dog Breed is {BREEDS[np.argmax(Y_pred)]}"))
